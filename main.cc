e66fd932700dee69c42b5822c36a8b6cb0b6b8fe
JoaoPito <joao.pito@hotmail.com>
2023-04-08 19:17:46 +0100

    Updated image

--- main.cc
+++ main.cc
@@ -36,7 +36,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 1280;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 150;
     const int max_depth = 50; // Max ray bounces
 
     // World
@@ -44,15 +44,15 @@ int main() {
     hittable_list world;
 
     // pink: color(0.94, 0.05, 0.53) | blue: color(0.05, 0.53, 0.94) | green: color(0.53, 0.94, 0.05)
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_ground = make_shared<lambertian>(color(0.73, 0.98, 0.25));
     auto material_center = make_shared<lambertian>(color(0.94, 0.15, 0.63));
     auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color(0.05, 0.53, 0.94), 0.25);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(-0.55, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.5), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -2.0), -0.45, material_left));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -2.0), 0.45, material_left));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -2.0), -0.4, material_left));
     world.add(make_shared<sphere>(point3(0.55, 0.0, -1.0), 0.5, material_right));
     
     // Camera
