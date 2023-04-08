ec9b5dac8ec41939bbd09a6ebb5eb3d4203bcc61
JoaoPito <joao.pito@hotmail.com>
2023-04-08 10:04:30 +0100

    Samples and anti-aliasing, also refactored the camera code and added utility functions

--- main.cc
+++ main.cc
@@ -3,13 +3,14 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 
 color ray_color(const ray& r, const hittable& world) {
     hit_record rec;
     if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+        return 0.5 * (rec.normal + color(1,1,1)); // Returns the normal vector translated to rgb
     }
 
     // Background
@@ -21,42 +22,37 @@ color ray_color(const ray& r, const hittable& world) {
 int main() {
 
     // Image setup
-
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
-    
     hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(-2,0,-2), 0.5));
+    world.add(make_shared<sphere>(point3(2,0,-2), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     // Camera
-
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0; // The distance between the camera origin and the viewport
-
-    auto origin = point3(0,0,0); // Camera origin point
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+    camera cam;
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for(int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         
         for(int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width-1);
-            auto v = double(j) / (image_height-1);
+            color pixel_color(0, 0, 0);
 
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
